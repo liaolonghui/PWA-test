d0b34f1b35ee7726598734226ced514cc17d6997
liaolonghui <1340881930@qq.com>
2021-10-07 20:14:33 +0800

    service worker的生命周期

--- 3.serviceWorker/sw.js
+++ 3.serviceWorker/sw.js
@@ -1,1 +1,14 @@
-console.log('555')
+self.addEventListener('install', event => {
+  console.log('install', event)
+  // 跳过等待
+  event.waitUntil(self.skipWaiting())
+})
+
+self.addEventListener('activate', event => {
+  console.log('activate', event)
+  event.waitUntil(self.clients.claim())
+})
+
+self.addEventListener('fetch', event => {
+  console.log('fetch', event)
+})
